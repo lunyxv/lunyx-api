c4f1d79e5ebf1a72aa014e89da239f914e422e7f
luny <luarmorv2@gmail.com>
2026-08-12 23:16:51 -0400

    Create get-data.js

---
+++ api/get-data.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async (req, res) => {
+    if (req.method !== 'GET') {
+        return res.status(405).json({ error: 'Method not allowed' });
+    }
+    
+    const { id } = req.query;
+    
+    if (!id) {
+        return res.status(400).json({ error: 'Missing ID' });
+    }
+    
+    const filePath = path.join(process.cwd(), 'data', `${id}.json`);
+    
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({ error: 'Data not found' });
+    }
+    
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    
+    res.status(200).json(data);
+};
